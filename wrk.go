71ca8112b7259c791831190f1e243b6e6eb43cec
MicroChang <striblingqsq@gmail.com>
2025-04-17 13:33:07 +0800

    feat(wrk):output histogram and result

--- wrk.go
+++ wrk.go
@@ -10,6 +10,8 @@ import (
 	"syscall"
 	"time"
 
+	histogram "github.com/HdrHistogram/hdrhistogram-go"
+
 	"github.com/crazyfrankie/wrk/client"
 )
 
@@ -36,7 +38,7 @@ var (
 )
 
 func initFlag() {
-	flag.BoolVar(&versionFlag, "version", false, "Print wrk version")
+	flag.BoolVar(&versionFlag, "v", false, "Print wrk version")
 	flag.BoolVar(&helpFlag, "help", false, "Print Help")
 	flag.BoolVar(&http2, "http2", true, "Use HTTP/2")
 	flag.IntVar(&goroutines, "c", 10, "Number of goroutines to use (concurrent connections)")
@@ -53,6 +55,8 @@ func initFlag() {
 }
 
 func main() {
+	initFlag()
+
 	flag.Parse()
 
 	quit := make(chan os.Signal, 1)
@@ -113,7 +117,64 @@ func main() {
 		go req.Run()
 	}
 
-	// TODO
+	responders := 0
+	stats := client.RequestStats{ErrMap: make(map[string]int), Histogram: histogram.New(1, int64(duration*1000000), 4)}
+
+	for responders < goroutines {
+		select {
+		case <-quit:
+			req.Stop()
+			fmt.Printf("stopping...\n")
+		case res := <-req.Stats:
+			stats.NumErrs += res.NumErrs
+			stats.NumRequests += res.NumRequests
+			stats.TotalRespSize += res.TotalRespSize
+			stats.TotalDuration += res.TotalDuration
+			responders++
+			for k, v := range res.ErrMap {
+				stats.ErrMap[k] = v
+			}
+			stats.Histogram.Merge(res.Histogram)
+		}
+	}
+
+	duration := time.Now().Sub(start)
+
+	if stats.NumRequests == 0 {
+		fmt.Println("Error: No statistics collected / no requests found")
+		fmt.Printf("Number of Errors:\t%v\n", stats.NumErrs)
+		if stats.NumErrs > 0 {
+			fmt.Printf("Error Counts:\t\t%v\n", convertErr(stats.ErrMap))
+		}
+		return
+	}
+
+	avgGoroutineDur := stats.TotalDuration / time.Duration(responders)
+
+	reqRate := float64(stats.NumRequests) / avgGoroutineDur.Seconds()
+	bytesRate := float64(stats.TotalRespSize) / avgGoroutineDur.Seconds()
+
+	overallReqRate := float64(stats.NumRequests) / duration.Seconds()
+	overallBytesRate := float64(stats.TotalRespSize) / duration.Seconds()
+
+	fmt.Printf("%v request in %v,%v read\n", stats.NumRequests, reqRate, Bytes{Size: float64(stats.TotalRespSize)})
+	fmt.Printf("Request/sec:\t\t%.2f\nTransfer/sec:\t\t%v\n", reqRate, Bytes{Size: bytesRate})
+	fmt.Printf("Overall Requests/sec:\t%.2f\nOverall Transfer/sec:\t%v\n", overallReqRate, Bytes{Size: overallBytesRate})
+	fmt.Printf("Fastest Request:\t%v\n", convertDuration(stats.Histogram.Min()))
+	fmt.Printf("Avg Req Time:\t\t%v\n", convertDuration(int64(stats.Histogram.Mean())))
+	fmt.Printf("Slowest Request:\t%v\n", convertDuration(stats.Histogram.Max()))
+	fmt.Printf("Number of Errors:\t%v\n", stats.NumErrs)
+	if stats.NumErrs > 0 {
+		fmt.Printf("Error Counts:\t\t%v\n", convertErr(stats.ErrMap))
+	}
+	fmt.Printf("10%%:\t\t\t%v\n", convertDuration(stats.Histogram.ValueAtPercentile(.10)))
+	fmt.Printf("50%%:\t\t\t%v\n", convertDuration(stats.Histogram.ValueAtPercentile(.50)))
+	fmt.Printf("75%%:\t\t\t%v\n", convertDuration(stats.Histogram.ValueAtPercentile(.75)))
+	fmt.Printf("99%%:\t\t\t%v\n", convertDuration(stats.Histogram.ValueAtPercentile(.99)))
+	fmt.Printf("99.9%%:\t\t\t%v\n", convertDuration(stats.Histogram.ValueAtPercentile(.999)))
+	fmt.Printf("99.9999%%:\t\t%v\n", convertDuration(stats.Histogram.ValueAtPercentile(.999999)))
+	fmt.Printf("99.99999%%:\t\t%v\n", convertDuration(stats.Histogram.ValueAtPercentile(.9999999)))
+	fmt.Printf("stddev:\t\t\t%v\n", convertDuration(int64(stats.Histogram.StdDev())))
 }
 
 // printDefaults is used when users enter -help or do not enter the url correctly
@@ -123,4 +184,16 @@ func printDefaults() {
 	flag.VisitAll(func(f *flag.Flag) {
 		fmt.Println("\t-"+f.Name, "\t", f.Usage, "(Default ", f.DefValue+")")
 	})
+}
+
+func convertErr(err map[string]int) string {
+	s := make([]string, 0, len(err))
+	for k, v := range err {
+		s = append(s, fmt.Sprint(k, "=", v))
+	}
+	return strings.Join(s, ",")
+}
+
+func convertDuration(usecs int64) time.Duration {
+	return time.Duration(usecs * 1000)
 }
